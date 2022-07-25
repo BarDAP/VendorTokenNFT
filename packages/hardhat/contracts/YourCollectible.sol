e221e35a63dfe41c0ff82445845322b92529f123
yanivlav <yan465@gamil.com>
2022-07-25 21:36:54 +0300

    update artwork

--- packages/hardhat/contracts/YourCollectible.sol
+++ packages/hardhat/contracts/YourCollectible.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "./YourToken.sol";
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
 
 
 contract YourCollectible is ERC721URIStorage {
@@ -12,13 +13,21 @@ contract YourCollectible is ERC721URIStorage {
     Counters.Counter private _tokenIds;
     YourToken yourToken;
 
-  constructor(address tokenAddress, bytes32[] memory assetsForSale) public ERC721("YourCollectible", "YCB") {
-      yourToken = YourToken(tokenAddress);
-    for(uint256 i=0;i<assetsForSale.length;i++){
-      forSale[assetsForSale[i]] = true;
+  constructor(address tokenAddress, bytes32[] memory assetsForSale)
+        public
+        ERC721("YourCollectible", "YCB")
+    {
+        _baseURI();
+        yourToken = YourToken(tokenAddress);
+        for (uint256 i = 0; i < assetsForSale.length; i++) {
+            forSale[assetsForSale[i]] = true;
+        }
     }
-  }
 
+    //function _baseURI() internal pure override returns (string memory) {
+    //  return "https://ipfs.io/ipfs/";
+    //}
+	
     //this marks an item in IPFS as "forsale"
     mapping (bytes32 => bool) public forSale;
 
